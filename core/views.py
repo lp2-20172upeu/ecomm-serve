62b515610a675dcddab6ffe7ca861167a292510f
Angel Sullon <asullom@gmail.com>
2017-10-04 15:36:10 -0500

    api rest user

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework import serializers, viewsets
 
-# Create your views here.
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        #fields = '__all__'
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
