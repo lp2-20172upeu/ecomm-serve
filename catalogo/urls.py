f55f2d1d8052917f0c01ea45995b34884d27ff71
Angel Sullon <asullom@gmail.com>
2017-11-22 15:40:47 -0500

    pagination con Producto

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,8 +2,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from .views.categoria_view import CategoriaViewSet
+from .views.producto_view import ProductoViewSet
 
 router = routers.DefaultRouter()
+router.register(r'productos', ProductoViewSet)
 router.register(r'categorias', CategoriaViewSet)
 
 urlpatterns = [
